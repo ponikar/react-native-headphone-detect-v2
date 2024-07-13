02ea4c3d919702722b42e87bb0096837019b1fcc
Darshan Ponikar <drsofficial3k@gmail.com>
2024-07-13 11:58:12 +0530

    feat: ios implementation done

--- ios/HeadphoneDetectV2.h
+++ ios/HeadphoneDetectV2.h
@@ -1,12 +1,13 @@
 
 #ifdef RCT_NEW_ARCH_ENABLED
 #import "RNHeadphoneDetectV2Spec.h"
-
 @interface HeadphoneDetectV2 : NSObject <NativeHeadphoneDetectV2Spec>
 #else
-#import <React/RCTBridgeModule.h>
 
-@interface HeadphoneDetectV2 : NSObject <RCTBridgeModule>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+@interface HeadphoneDetectV2 : RCTEventEmitter <RCTBridgeModule>
 #endif
 
 @end
--- ios/HeadphoneDetectV2.mm
+++ ios/HeadphoneDetectV2.mm
@@ -1,18 +1,104 @@
 #import "HeadphoneDetectV2.h"
+#import <AVFoundation/AVFoundation.h>
+#import <React/RCTEventEmitter.h>
+
+#import <React/RCTBridge.h>
+#import <React/RCTEventDispatcher.h>
+
 
 @implementation HeadphoneDetectV2
+{
+    BOOL hasListeners;
+}
 RCT_EXPORT_MODULE()
 
-// Example method
-// See // https://reactnative.dev/docs/native-modules-ios
-RCT_EXPORT_METHOD(multiply:(double)a
-                  b:(double)b
-                  resolve:(RCTPromiseResolveBlock)resolve
-                  reject:(RCTPromiseRejectBlock)reject)
-{
-    NSNumber *result = @(a * b);
+static NSString * const AUDIO_DEVICE_CHANGED_NOTIFICATION = @"AUDIO_DEVICE_CHANGED_NOTIFICATION";
+static NSString * const IS_AUDIO_DEVICE_CONNECTED = @"isAudioDeviceConnected";
 
-    resolve(result);
+
+- (instancetype) init
+{
+    if (self = [super init]) {
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(audioRouteChangeListenerCallback:) name:AVAudioSessionRouteChangeNotification object:nil];
+    }
+    return self;
+}
+
+- (void) dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
++ (BOOL) requiresMainQueueSetup
+{
+    return YES;
+}
+
+- (dispatch_queue_t) methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
+- (NSDictionary *)constantsToExport
+{
+    return @{ @"AUDIO_DEVICE_CHANGED_NOTIFICATION": AUDIO_DEVICE_CHANGED_NOTIFICATION };
+}
+
+-(void) startObserving
+{
+    hasListeners = YES;
+}
+
+-(void) stopObserving
+{
+    hasListeners = NO;
+}
+
+- (NSArray<NSString *> *) supportedEvents
+{
+    return @[AUDIO_DEVICE_CHANGED_NOTIFICATION];
+}
+
+- (void) audioRouteChangeListenerCallback:(NSNotification*)notification
+{
+    if (hasListeners) { // Only send events if anyone is listening
+        NSDictionary * res = [HeadphoneDetectV2 isAudioDeviceConnected];
+        [self sendEventWithName:AUDIO_DEVICE_CHANGED_NOTIFICATION
+                           body: res
+         ];
+    }
+}
+
++ (NSDictionary *) isAudioDeviceConnected
+{
+    NSMutableDictionary *res = [
+                                @{ @"audioJack": @NO, @"bluetooth": @NO }
+                                mutableCopy
+                                ];
+
+    AVAudioSessionRouteDescription* route = [[AVAudioSession sharedInstance] currentRoute];
+
+    for (AVAudioSessionPortDescription* desc in [route outputs]) {
+        if ([[desc portType] isEqualToString:AVAudioSessionPortHeadphones]) {
+            res[@"audioJack"] = @YES;
+        }
+
+        if (
+            [[desc portType] isEqualToString:AVAudioSessionPortBluetoothA2DP] ||
+            [[desc portType] isEqualToString:AVAudioSessionPortBluetoothHFP] ||
+            [[desc portType] isEqualToString:AVAudioSessionPortBluetoothLE]
+            ) {
+            res[@"bluetooth"] = @YES;
+        }
+    }
+
+    return res;
+}
+
+RCT_EXPORT_METHOD(isAudioDeviceConnected:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+    resolve([HeadphoneDetectV2 isAudioDeviceConnected]);
 }
 
 
